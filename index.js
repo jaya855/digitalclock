9012adc5366e4b3ce7007e623afdee9f1859ac21
jaya855 <srivastavajaya175@gmail.com>
2021-12-16 13:15:01 +0530

    digitalclock

---
+++ index.js
@@ -0,0 +1,105 @@
+// alert("me");
+// document.write("this is a document")     
+//  2. javascript console API
+/* console.log("hello world",4+5,"hii all");
+console.warn("this is a warning");
+  console.error("this is an error");*/
+ /* numbers
+  var number1=5;
+  var number2=10;
+  console.log(number1+number2);*/
+ /*strings
+  var str1="this is jaya";
+  console.log(str1);*/
+/* objects
+var marks={ravi:30,shubh:50,rekha:80,tina:90};
+ console.log(marks);*/
+/* Booleans
+var a=true;
+var b=false;
+console.log(a,b);*/
+/* var und=undefined;
+ console.log(und);
+var und;
+console.log(und);*/
+/*var n=null;
+ console.log(n);*/
+/* at a very high level there are two types of data types
+ 1. primitive data types:Boolean,string,number,null,undefined,symbol
+ 2. reference data types:objects,arrays*/
+// arrays
+/*var arr=[1,2,3,"bablu",4,5];
+console.log(arr);
+console.log(arr[2]);
+console.log(arr[3]);*/
+// arithmetic operators
+var a=20;
+var b=40;
+// console.log("the sum of a and b is",a+b);
+// console.log("the diff of a and b is",a-b);
+// console.log("the product of a and b is",a*b);
+// console.log("the quotient of a and b is",a/b);
+ /*assignment operators
+var c=b;
+console.log(c);
+c+=2;
+c-=2;
+console.log(c);*/
+/*comparison operator
+var x=6;
+var y=8;
+console.log(x==y);*/
+ /*logical and 
+console.log(true&&true);
+console.log(true&&false);*/
+/*logical or
+console.log(true||true);
+console.log(true||false);*/
+/* logical not
+console.log(!true);
+console.log(!false);*/
+/* functions in javascript
+function avg(a,b){
+    return (a+b)/2;
+}
+c1=avg(2,4);
+c2=avg(2,6);
+console.log(c1,c2);*/
+/*var s=10;
+if(s>15){
+  console.log("u r a kid");
+}
+else{
+  console.log("keep mum");
+}*/
+//  loops
+// var arr=[1,2,3,4,5,6];
+/*for(var i=0;i<arr.length;i++){
+  console.log(arr[i]);
+}*/
+/*arr.forEach(function(element){
+    console.log(arr[element]);
+})*/
+// let a=0;
+// const b=0;
+/*var j=0;
+while(j<arr.length){
+  console.log(arr[j]);
+  j++;
+}*/
+/*var j=0;
+do{
+ console.log(arr[j]); 
+}while(j<arr.length);*/
+let arr1=["lucky","didu",5,null,"hello",true];
+// console.log(arr1.length);
+//  console.log(arr1);
+// arr1.pop();
+// console.log(arr1);
+//  arr1.push("harry");
+//  console.log(arr1);
+// arr1.shift();
+// arr1.unshift("ritik");
+// const c=arr1.unshift("ritik");
+// console.log(c);
+// console.log(arr1);
